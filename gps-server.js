34cd4efe68d78785a343f2a58ceebc3a07636021
ariezieq <ariezieq123@gmail.com>
2026-01-13 10:15:12 +0700

    fix: ignore env file

--- gps-server.js
+++ gps-server.js
@@ -13,7 +13,7 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
